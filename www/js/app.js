4085f5f0865eb8140f329bf2af71576ef16e7b24
Jackmew <wonderfuljack123@gmail.com>
2015-11-25 11:17:16 +0800

    display add result

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,15 @@
 
 var app = angular.module('starter', ['ionic'])
 
+app.controller("PromiseCtrl", function($scope) {
+
+  function add(x, y) {
+    return x + y ;
+  }
+
+  $scope.result = add(5, 2) ;
+});
+
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
